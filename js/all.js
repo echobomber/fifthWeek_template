83fe8c8d0e4a8badd1763c6b280044599a6b1e6d
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2021-03-10 23:16:16 +0800

    03/10 串接測試

--- js/all.js
+++ js/all.js
@@ -3,7 +3,7 @@ let ticketImg = document.getElementById('ticketImgUrl');
 let ticketRegion = document.getElementById('ticketRegion');
 let ticketPrice = document.getElementById('ticketPrice');
 let ticketNum = document.getElementById('ticketNum');
-let ticketRank = document.getElementById('ticketRank');
+let ticketRate = document.getElementById('ticketRate');
 let ticketDescription = document.getElementById('ticketDescription');
 // 
 let addTicketBtn = document.querySelector('.addTicket-btn');
@@ -30,27 +30,43 @@ let showTicketCard = (data) => {
     data.forEach((item) => {
         str +=
             `
-        <li class="tickeCard-item">
-            <div class="tickeCard-img">
-                <p class="img-caption">
-                    ${item.area}
-                </p>
-                <img src="${item.imgUrl}" alt="">
+        <li class="ticketCard">
+            <div class="ticketCard-img">
+                <a href="#">
+                    <img src="${item.imgUrl}" alt="">
+                </a>
+                <div class="ticketCard-region">${item.area}</div>
+                <div class="ticketCard-rank">${item.rate}</div>
             </div>
-            <div class="tickeCard-text">
-                <h2>${item.name}</h2>
-                <p class="tickeCard-description">${item.description}</p>
-                <p class="tickeCard-num">剩下倒數 ${item.group} 組套票 </p>
-                <p class="tickeCard-price">$${item.price}</p>
+            <div class="ticketCard-content">
+                <div>
+                    <h3>
+                        <a href="#" class="ticketCard-name">${item.name}</a>
+                    </h3>
+                    <p class="ticketCard-description">
+                        ${item.description}
+                    </p>
+                </div>
+                <div class="ticketCard-info">
+                    <div class="ticketCard-num">
+                        <p>
+                            <span><i class="fas fa-exclamation-circle"></i></span>
+                            剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+                        </p>
+                    </div>
+                    <p class="ticketCard-price">
+                        TWD <span id="ticketCard-price">$${item.price}</span>
+                    </p>
+                </div>
             </div>
         </li>
         `;
     })
     ticketCard.innerHTML = str;
     if (data.length > 0) {
-        searchResultText.textContent = `搜尋資料為 ${data.length} 筆`;
+        searchResultText.textContent = `本次搜尋共 ${data.length} 筆資料`;
     } else {
-        searchResultText.textContent = `搜尋資料為 ${data.length} 筆，無符合項目`;
+        searchResultText.textContent = `本次搜尋共 ${data.length} 筆資料，無符合項目`;
     }
 }
 
@@ -62,119 +78,43 @@ let addTicketData = () => {
         area: ticketRegion.value,
         description: ticketDescription.value,
         group: ticketNum.value,
-        price: ticketPrice.value
+        price: ticketPrice.value,
+        rate: ticketRate.value
     };
     ticketData = JSON.parse(localStorage.getItem('ticketData')) || remoteData;
     ticketData.push(tempData);
     localStorage.setItem('ticketData', JSON.stringify(ticketData));
     showTicketCard(ticketData);
+    document.querySelector('.cantFind-area').setAttribute("style", "display: none;");
+    ticketCard.setAttribute("style", "display: flex;");
+    
     ticketName.value = "";
     ticketImg.value = "";
     ticketRegion.value = "";
-    ticketDescription.value = "";
-    ticketNum.value = "";
     ticketPrice.value = "";
+    ticketNum.value = "";
+    ticketRate.value = "";
+    ticketDescription.value = "";
 }
 
-let keywordSearch = (e) => {
-    e.preventDefault();
-    let keyword = keywordInput.value.trim();
-    ticketData = JSON.parse(localStorage.getItem('ticketData')) || remoteData;
-    ticketData = ticketData.filter((item) => {
-        return (item.name.match(keyword));
-    });
-    showTicketCard(ticketData);
-}
 let areaSearch = (e) => {
     let area = regionSelect.value.trim();
     ticketData = JSON.parse(localStorage.getItem('ticketData')) || remoteData;
-    // 要給 C3.js 的資料
-    let chartTitle = "地區數量比例";
-    let areaNumData = [];
-    // 篩出所有地區名稱
-    let filterArea = ticketData.map((item) => {
-        return item.area;
-    })
-    // 篩出不重複的地區名稱
-    filterArea = filterArea.filter((element, index, self) => {
-        return self.indexOf(element) === index;
-    });
-    // 計算各自的數量
-    filterArea.forEach((item) => {
-        let Num = 0;
-        for(let i = 0; i < ticketData.length; i++){
-            if(item === ticketData[i].area){
-                Num++;
-            }
-        }
-        areaNumData.push([item, Num]);
-    })
-    callChart(chartTitle, areaNumData);
     // 秀卡片
     ticketData = ticketData.filter((item) => {
         return (item.area.match(area));
     });
-    showTicketCard(ticketData);
-}
-
-let priceSearch = (e) => {
-    let price = priceSelect.value.trim();
-    ticketData = JSON.parse(localStorage.getItem('ticketData')) || remoteData;
-    // 要給 C3.js 的資料
-    let chartTitle = "價格區間比例";
-    let priceNumData = [];
-    let Num1 = 0, Num2 = 0, Num3 = 0;
-    for(let i = 0; i < ticketData.length; i++){
-        if(ticketData[i].price <= 500){
-            Num1++;
-        }else if(ticketData[i].price > 500 && ticketData[i].price < 1000){
-            Num2++;
-        }else if (ticketData[i].price >= 1000){
-            Num3++;
-        }
-    }
-    console.log(Num1, Num2, Num3)
-    priceNumData.push(['500以下', Num1]);
-    priceNumData.push(['500 - 1000', Num2]);
-    priceNumData.push(['1000 以上', Num3]);
-    callChart(chartTitle, priceNumData);
-    // 
-    if (price === "500以下") {
-        ticketData = ticketData.filter((item) => {
-            return (item.price <= 500);
-        });
-    } else if (price === "500-1000") {
-        ticketData = ticketData.filter((item) => {
-            return (item.price > 500 && item.price < 1000);
-        });
-    } else if (price === "1000以上") {
-        ticketData = ticketData.filter((item) => {
-            return (item.price >= 1000);
-        });
+    if(ticketData.length > 0) {
+        ticketCard.setAttribute("style", "display: flex;");
+        document.querySelector('.cantFind-area').setAttribute("style", "display: none;");
+    }else{
+        ticketCard.setAttribute("style", "display: none;");
+        document.querySelector('.cantFind-area').setAttribute("style", "display: block;");
     }
     showTicketCard(ticketData);
-}
-
-// c3.js
-let callChart = (chartTitle, NumData) => {    
-    let chart = c3.generate({
-        data: {
-            columns: NumData,
-            type: 'donut',
-            onclick: function (d, i) { console.log("onclick", d, i); },
-            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-        },
-        donut: {
-            title: chartTitle
-        },
-        bindto: '#NumChart'
-    });
 }
 
 regionSelect.addEventListener('change', areaSearch, false);
-priceSelect.addEventListener('change', priceSearch, false);
-searchBtn.addEventListener('click', keywordSearch, false);
 
 // validate.js
 ; (function () {
@@ -204,6 +144,12 @@ searchBtn.addEventListener('click', keywordSearch, false);
             },
 
         },
+        "套票星級": {
+            presence: {
+                message: "是必填的欄位"
+            },
+
+        },
         "套票組數": {
             presence: {
                 message: "是必填的欄位"
@@ -225,9 +171,9 @@ searchBtn.addEventListener('click', keywordSearch, false);
     };
 
     // 綁定 Form 表單，在送出前做欄位檢查
-    let form = document.querySelector('#addTicketForm');
-    let inputs = document.querySelectorAll(".addTicket-form-item input, .addTicket-form-item select, .addTicket-form-item textarea");
-    let messages = document.querySelectorAll('[data-message]');
+    let form = document.querySelector('.addTicket-form');
+    let inputs = document.querySelectorAll(".addTicket-input input, .addTicket-input select, .addTicket-input textarea");
+    let messages = document.querySelectorAll('.alert-message p');
     form.addEventListener("submit", handleFormSubmit, false);
 
     // 沒有錯誤就顯示成功傳送
@@ -240,6 +186,7 @@ searchBtn.addEventListener('click', keywordSearch, false);
         } else {
             addTicketData();
         }
+        // console.log(messages);
     }
     // 監聽 input 值改變的狀況
     inputs.forEach((item) => {
@@ -247,21 +194,30 @@ searchBtn.addEventListener('click', keywordSearch, false);
             e.preventDefault();
             let targetName = item.name;
             let errors = validate(form, constraints);
-            let str = `[data-message="${targetName}"]`;
-            if (errors) {
+            let str = `${item.id}-message`;
+            console.log(errors[targetName]);
+            if (errors[targetName]) {
                 // 取得要輸出錯誤訊息的 p 標籤
-                document.querySelector(str).textContent = errors[targetName];
+                document.querySelector(`#${str}`).innerHTML = 
+                `
+                    <i class="fas fa-exclamation-circle"></i>
+                    <span>${errors[targetName]}</span>
+                `;
             } else {
-                document.querySelector(str).textContent = "";
+                document.querySelector(`#${str}`).innerHTML = "";
             }
-
         });
     })
     // 更新錯誤訊息
     function showErrors(errors) {
         messages.forEach((item) => {
-            item.textContent = "";
-            item.textContent = errors[item.dataset.message];
+            item.innerHTML = "";
+            if(errors[item.dataset.message]){
+                item.innerHTML = `
+                <i class="fas fa-exclamation-circle"></i>
+                <span>${errors[item.dataset.message]}</span>
+            `;
+            }
         })
     }
 })();
